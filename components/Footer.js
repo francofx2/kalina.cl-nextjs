efe45afb709512af2f50f3d928898a1ae08e0c68
francofx2 <81722620+francofx2@users.noreply.github.com>
2022-11-21 20:25:33 -0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with Love by Franco Levera
+        Made with &#128420; by Franco Levera
       </footer>
     </>
   )
