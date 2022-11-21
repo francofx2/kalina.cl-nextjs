905837550158d3d037dd1269fca7d0233c827fa2
francofx2 <francofx2@gmail.com>
2022-11-21 19:29:56 -0300

    first edits

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kalina</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Manifesto" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Descubre <code>kalina.cl</code>
         </p>
       </main>
 
