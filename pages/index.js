6b2a107f07269a3c45c9d7e3f709d98a1b11088f
francofx2 <81722620+francofx2@users.noreply.github.com>
2022-11-21 19:50:48 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,8 +44,9 @@ export default function Home() {
           “Nor is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure.”
           McClintock's eye for detail certainly helped narrow the whereabouts of lorem ipsum's origin, however, the “how and when” still remain something of a mystery, with competing theories and timelines.
         </p>
-
+       </main>
       <Footer />
+            
     </div>
-  );
+  )
 }
